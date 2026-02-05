6008e8992b0b0d8b4ddc217ebb3259455c6e082d
xsfunc <xsfunc@gmail.com>
2026-02-05 18:28:06 +0300

    Add tests for export formatting

--- internal/app/app.go
+++ internal/app/app.go
@@ -206,76 +206,9 @@ func (a *App) Run(ctx context.Context, opts RunOptions) error {
 		messages[i], messages[opp] = messages[opp], messages[i]
 	}
 
-	type messageBlock struct {
-		SenderID int64
-		Start    time.Time
-		End      time.Time
-		Lines    []string
-	}
-
-	formatSenderID := func(id int64) string {
-		if id <= 0 {
-			return "id=unknown"
-		}
-		return fmt.Sprintf("id=%d", id)
-	}
-
-	normalizeLines := func(text string) []string {
-		normalized := strings.ReplaceAll(text, "\r\n", "\n")
-		normalized = strings.ReplaceAll(normalized, "\r", "\n")
-		normalized = strings.TrimSpace(normalized)
-		if normalized == "" {
-			return nil
-		}
-		rawLines := strings.Split(normalized, "\n")
-		lines := make([]string, 0, len(rawLines))
-		for _, line := range rawLines {
-			line = strings.TrimSpace(line)
-			if line == "" {
-				continue
-			}
-			lines = append(lines, line)
-		}
-		return lines
-	}
-
-	var blocks []messageBlock
-	for _, msg := range messages {
-		lines := normalizeLines(msg.Text)
-		if len(lines) == 0 {
-			continue
-		}
-		if len(blocks) == 0 || blocks[len(blocks)-1].SenderID != msg.SenderID {
-			blocks = append(blocks, messageBlock{
-				SenderID: msg.SenderID,
-				Start:    msg.Date,
-				End:      msg.Date,
-				Lines:    lines,
-			})
-			continue
-		}
-		last := &blocks[len(blocks)-1]
-		last.End = msg.Date
-		last.Lines = append(last.Lines, lines...)
-	}
-
-	for _, block := range blocks {
-		start := block.Start.Format("15:04")
-		end := block.End.Format("15:04")
-		timeLabel := start
-		if end != start {
-			timeLabel = fmt.Sprintf("%s-%s", start, end)
-		}
-		if _, err := fmt.Fprintf(f, "[%s] %s:\n", timeLabel, formatSenderID(block.SenderID)); err != nil {
-			fmt.Printf("failed to write message block: %v\n", err)
-			continue
-		}
-		for _, line := range block.Lines {
-			if _, err := fmt.Fprintf(f, "  %s\n", line); err != nil {
-				fmt.Printf("failed to write message line: %v\n", err)
-				break
-			}
-		}
+	blocks := buildMessageBlocks(messages)
+	if err := writeMessageBlocks(f, blocks); err != nil {
+		fmt.Printf("failed to write message blocks: %v\n", err)
 	}
 
 	fmt.Printf("Successfully exported %d messages to %s\n", len(messages), filename)
